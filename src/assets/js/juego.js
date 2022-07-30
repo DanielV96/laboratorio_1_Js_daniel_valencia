54a794d962bc8753e602df83788fc67a40c7169f
Daniel <el.dani72@gmail.com>
2022-07-29 21:35:25 -0500

    Segundo commit

--- src/assets/js/juego.js
+++ src/assets/js/juego.js
@@ -9,6 +9,18 @@ let baraja = []
 const tipos = ['C', 'D', 'H', 'S']
 const letras = ['J', 'Q', 'K', 'A']
 
+let playerPoints = 0
+let pcPoints = 0
+
+// Referencias al Html
+const btnPedir = document.querySelector('#btn-pedir')
+// const smallPuntajeJugador = document.querySelector('small')
+const btnDetener = document.querySelector('#btn-detener')
+const btnNuevo = document.querySelector('#btn-nuevo')
+const puntosHtml = document.querySelectorAll('small')
+const divJugadorCartas = document.querySelector('#player-cards')
+divComputadoraCartas = document.querySelector('#pc-cards')
+
 const crearBaraja = () => {
   //Se puebla el arreglo con los números y tipos de la baraja
   for (let i = 2; i <= 10; i++) {
@@ -28,18 +40,19 @@ const crearBaraja = () => {
 
 crearBaraja()
 
-console, console.log(baraja)
+// console.log(baraja)
 
 //Esta función pide una carta
 const pedirCarta = () => {
-  if (baraja.length === 0) throw 'No hay cartas en la baraja'
+  const barajaTamanio = baraja.length
+  if (barajaTamanio === 0) throw 'No hay cartas en la baraja'
 
-  const index = Math.floor(Math.random() * 51)
-  console.log({ index })
+  const index = Math.floor(Math.random() * barajaTamanio)
+  // console.log({ index })
 
   const carta = baraja.splice(index, 1)[0]
-  console.log({ carta })
-  console.log({ baraja })
+  // console.log({ carta })
+  // console.log({ baraja })
   return carta
 }
 
@@ -49,5 +62,72 @@ const valorCarta = (carta) => {
   const valor = carta.substring(0, carta.length - 1)
   return isNaN(valor) ? (valor === 'A' ? 11 : 10) : Number(valor)
 }
+// console.log(valorCarta(pedirCarta()))
 
-console.log(valorCarta(pedirCarta()))
+// Turno de la computadora
+const turnoComputadora = (puntosMinimos) => {
+  do {
+    const carta = pedirCarta()
+    pcPoints += valorCarta(carta)
+    puntosHtml[1].innerText = pcPoints
+
+    const imgCarta = document.createElement('img')
+    imgCarta.src = `assets/cartas/${carta}.png`
+    imgCarta.classList.add('carta')
+    divComputadoraCartas.append(imgCarta)
+    if (puntosMinimos > 21) break
+  } while (pcPoints <= puntosMinimos && puntosMinimos <= 21)
+
+  setTimeout(() => {
+    if (puntosMinimos === pcPoints) alert('Hubo empate')
+    else if (puntosMinimos > 21) alert('Pc Win')
+    else if (pcPoints > 21) alert('perfect! you Win')
+    else alert('Pc Win')
+  }, 100)
+}
+
+// Eventos
+btnPedir.addEventListener('click', () => {
+  const carta = pedirCarta()
+  // console.log({ carta })
+  playerPoints += valorCarta(carta)
+  console.log({ playerPoints })
+  // smallPuntajeJugador.innerText = playerPoints
+  puntosHtml[0].innerText = playerPoints
+  // <img class="carta" src="assets/cartas/7C.png" alt="" />
+  const imgCarta = document.createElement('img')
+  imgCarta.src = `assets/cartas/${carta}.png`
+  imgCarta.classList.add('carta')
+  divJugadorCartas.append(imgCarta)
+
+  if (playerPoints > 21) {
+    btnPedir.disabled = true
+    btnDetener.disabled = true
+    console.warn('Sorry you lost')
+    turnoComputadora(playerPoints)
+  } else if (playerPoints === 21) {
+    btnPedir.disabled = true
+    btnDetener.disabled = true
+    console.log('21! You Won')
+    turnoComputadora(playerPoints)
+  }
+})
+
+btnDetener.addEventListener('click', () => {
+  btnPedir.disabled = true
+  btnDetener.disabled = true
+  turnoComputadora(playerPoints)
+})
+
+btnNuevo.addEventListener('click', () => {
+  playerPoints = 0
+  pcPoints = 0
+  btnPedir.disabled = false
+  btnDetener.disabled = false
+  puntosHtml[0].innerText = 0
+  puntosHtml[1].innerText = 0
+  divJugadorCartas.innerHTML = ''
+  divComputadoraCartas.innerHTML = ''
+  console.clear()
+  crearBaraja()
+})
